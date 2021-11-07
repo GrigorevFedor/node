c03360cd953363f714b6edd6e606d3d90064d31d
popapauka <gfavlg@yandex.ru>
2021-11-07 17:36:16 +0300

    l6

--- index.js
+++ index.js
@@ -1,80 +1,92 @@
-// 1) Решите задачу по выводу данных в консоль.
+const socket = require('socket.io');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// console.log('Record 1');
+// const { MongoClient } = require("mongodb");
 
-// setTimeout(() => {
-//     console.log('Record 2');
-//     Promise.resolve().then(() => {
-//         setTimeout(() => {
-//             сonsole.log('Record 3');
-//             Promise.resolve().then(() => {
-//                 console.log('Record 4');
-//             });
-//         });
-//     });
-// });
+// const uri =
+//     "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
-// console.log('Record 5');
+// const client = new MongoClient(uri);
+// async function run() {
+//     try {
+//         await client.connect();
+//         await client.db("admin").command({ ping: 1 });
+//         console.log("Connected successfully to server");
+//     } finally {
+//         await client.close();
+//     }
+// }
+// run().catch(console.dir);
 
-// Promise.resolve().then(() => Promise.resolve().then(() => console.log('Record 6')));
 
-// 1 - tick 1 poll
-// 5 - tick 1 poll
-// 6 - tick 1 close callbacks
-// 2 - tick 2 timers
-// 3 - tick 3 timers
-// 4 - tick 3 close callbacks
+const server = http
+    .createServer(((req, res) => {
+        const indexPath = path.join(__dirname, 'index.html');
+        const readStream = fs.createReadStream(indexPath);
 
-// 2) Напишите программу, которая будет принимать на вход 
-// несколько аргументов: дату и время в формате 
-// «час - день - месяц - год». Задача программы — 
-// создавать для каждого аргумента таймер с обратным 
-// отсчётом: посекундный вывод в терминал состояния 
-// таймеров(сколько осталось).По истечении какого - 
-// либо таймера, вместо сообщения о том, сколько осталось, 
-// требуется показать сообщение о завершении его работы.
-// Важно, чтобы работа программы основывалась на событиях.
+        readStream.pipe(res);
+    }));
 
-// $ node index.js 01-04-26-10-2021 01-24-27-10-2021
+const usersList = [];
 
-const luxon = require('luxon')
-
-const t1 = process.argv[2].split('-')
-const t2 = process.argv[3].split('-')
-
-let d1 = luxon.DateTime.fromISO(t1[4] + '-' + t1[3] + '-' + t1[2] + 'T' + t1[0] + ':' + t1[1]);
-let d2 = luxon.DateTime.fromISO(t2[4] + '-' + t2[3] + '-' + t2[2] + 'T' + t2[0] + ':' + t2[1]);
-
-const now = luxon.DateTime.now();
-
-let dur1 = luxon.Duration.fromMillis(d1 - now).shiftTo('years', 'months', 'days', 'minutes', 'seconds');
-let dur2 = luxon.Duration.fromMillis(d2 - now).shiftTo('years', 'months', 'days', 'minutes', 'seconds');
-
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
-
-const run = async (timer) => {
-    const delay = 1000;
-    timer.dur = timer.dur.minus({ seconds: 1 }).shiftTo('years', 'months', 'days', 'minutes', 'seconds').normalize();
-
-    emitter.emit('print', timer);
-    await new Promise(resolve => setTimeout(resolve, delay));
-    console.clear();
-    await run(timer);
+function getAlphaNumericRandom(len) {
+    if ((len == undefined) || (len <= 0)) { len = 1; }
+    var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var result = '';
+    var iffirst = 0;
+    for (var i = 0; i < len; i++) {
+        if (i == 0) { iffirst = 10; } else { iffirst = 0; }
+        result += characters[Math.round(Math.random() * (characters.length - iffirst - 1))];
+    }
+    return result;
 }
 
-class Handlers {
-    static printTime(timer) {
-        if (timer.dur.toMillis() > 0) {
-            console.log(timer.name + ': seconds:' + Math.floor(timer.dur.seconds) + ' minutes:' + timer.dur.minutes + ' hours:' + timer.dur.hours + ' days:' + timer.dur.days + ' months:' + timer.dur.months + ' years:' + timer.dur.years);
-        }
-        else {
-            console.log(timer.name + ': time is up');
-        }
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function createNewUser(id) {
+    return {
+        id: id,
+        name: getAlphaNumericRandom(5),
+        color: getRandomColor(),
     }
 }
 
-emitter.on('print', Handlers.printTime);
+const io = socket(server);
+io.on('connection', client => {
+    console.log('client - connect', client.id);
+    usersList.push(createNewUser(client.id))
+    client.broadcast.emit('server-connect', usersList.find(el => el.id == client.id)?.name);
+    client.broadcast.emit('refresh', usersList.length);
 
-run({ name: 'timer1', dur: dur1 });
-run({ name: 'timer2', dur: dur2 });
+    client.on('client-msg', data => {
+        const payload = {
+            message: data.message,
+        };
+
+        client.broadcast.emit('server-msg', payload);
+        client.emit('server-msg', payload);
+    });
+
+    client.on('reconnect', function () {
+        console.log('client - reconnect', usersList.find(el => el.id == client.id)?.name);
+        client.broadcast.emit('refresh', usersList.length);
+    });
+
+    client.on('disconnect', function () {
+        console.log('client - disconnect', client.id);
+        client.broadcast.emit('server-disconnect', usersList.find(el => el.id == client.id)?.name);
+        client.broadcast.emit('refresh', usersList.length);
+    });
+
+});
+
+server.listen(5555);
